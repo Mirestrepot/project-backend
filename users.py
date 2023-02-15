e0fd61b563454b9dbcdb8f539ea98b1ebc0099e2
Mirestrepot <mirestrepot@unal.edu.co>
2023-02-14 21:27:42 -0500

    updates HTTP status, API Post, Put, Delete, Operations

--- users.py
+++ users.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+
+ 
+#Pydantic
 from pydantic import BaseModel
-    
+ 
+#FastAPI
+from fastapi import FastAPI, HTTPException   
+from fastapi import status
 app = FastAPI()    
 
 
@@ -16,23 +21,67 @@ users_list = [
     User(id=1,first_name="Miguel 1",last_name="Restrepo",country="Colombia",age="23"),
     User(id=2,first_name="Miguel 2",last_name="Restrepo",country="Colombia",age="23"),
     User(id=3,first_name="Miguel 3",last_name="Restrepo",country="Colombia",age="23")
+    
     ]
 
 
-@app.get("/users")
+@app.get(
+    path="/users",
+    status_code=status.HTTP_200_OK)
 async def users():
     return users_list
 
 
 #Path
-@app.get("/user/{id}")
+@app.get(
+    path="/user/{id}",
+    status_code=status.HTTP_200_OK)
 async def users(id:int):
     return search_user(id)
 
 #Query
-@app.get("/user/")
-async def userquery(id:int):
+@app.get(
+    path="/user/",
+    status_code=status.HTTP_200_OK
+    )
+async def userquery(id: int):
     return search_user(id)
+
+@app.post(
+    path="/user/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=User)
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        HTTPException(status_code=404, detail="User already exists")
+    else:
+        users_list.append(user)
+
+@app.put(
+    path="/user/",
+    status_code=status.HTTP_200_OK)
+async def user(user:User):
+    found = False
+    for index ,saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        HTTPException(status_code=404, detail="User not exists")
+    
+@app.delete(
+    path="/user/{id}",
+    status_code=status.HTTP_200_OK)
+async def user(id: int):
+    found = False
+    for index ,saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+    if not found:
+        HTTPException(status_code=404, detail="User not exists")
+
+
 
 
 
@@ -41,5 +90,6 @@ def search_user(id:int):
     try: 
         return list(users)[0]
     except :
-        return  "That ID don't was in the data"   
+        HTTPException(status_code=204, detail="hat ID don't was in the data")   
     
+
